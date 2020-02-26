583f85bc926244e0c4e7e852b329ad07a61612ee
0num4 <oonuma.ryouyu@unipro.co.jp>
2020-02-27 03:13:16 +0900

    mockを返すように修正

--- lambda-apigateway/lambda_function.py
+++ lambda-apigateway/lambda_function.py
@@ -1,7 +1,31 @@
+import json
+
 def lambda_handler(event, context):
+    # TODO implement
+    mock = {
+      "words": [
+        {
+          "sentence_index": 0,
+          "begin": 0,
+          "end": 1,
+          "rubies": [
+            {
+              "partial_rubies": [
+                {
+                  "begin": 0,
+                  "end": 1,
+                  "surface": "角",
+                  "ruby": "すみ",
+                  "is_first": True
+                }
+              ],
+              "is_first": True
+            }
+          ]
+        }
+      ]
+    }
     return {
-        'isBase64Encoded': False,
         'statusCode': 200,
-        'headers': {},
-        'body': '{"message": "Hello from AWS Lambda"}'
+        'body': json.dumps(mock)
     }
